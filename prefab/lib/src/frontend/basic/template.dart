fa73ecd9757592eca53e948062fbbfa1a7ed612e
Stijn Van Bael <stijn.vanbael@enprove.be>
2023-10-25 20:52:58 +0200

    Add entity dialog (WIP)

--- prefab/lib/src/frontend/basic/template.dart
+++ prefab/lib/src/frontend/basic/template.dart
@@ -10,43 +10,61 @@ abstract class BasicFrontendTemplate {
 
   BasicFrontendTemplate(this.registry);
 
-  String page(String title, List<String> content) =>
-      h.html(lang: 'en', content: [
-        h.head([
-          h.meta(charset: 'utf-8'),
-          h.meta(httpEquiv: 'X-UA-Compatible', content: 'IE=edge'),
-          h.meta(
-            name: 'viewport',
-            content: 'width=device-width, initial-scale=1.0',
-          ),
-          h.title('Todos'),
-          h.link(
-            rel: h.LinkRel.stylesheet,
-            href:
-                'https://cdn.jsdelivr.net/npm/@shoelace-style/$shoelaceVersion/cdn/themes/light.css',
-          ),
-          h.link(
-              rel: h.LinkRel.stylesheet,
-              href: 'http://localhost:8081/styles/prefab.css'),
-          h.script(
-            type: 'module',
-            src:
-                'https://cdn.jsdelivr.net/npm/@shoelace-style/$shoelaceVersion/cdn/shoelace-autoloader.js',
-          ),
-          h.script(src: 'http://localhost:8081/interface.dart.js', defer: true),
-        ]),
-        h.body([
-          h.aside([
-            sl.menu(
-              items: menu(),
-            )
+  String page(String title, List<String> content) => h.html(
+        lang: 'en',
+        classes: ['sl-theme-dark'],
+        content: [
+          _head(),
+          h.body([
+            h.aside([
+              sl.menu(
+                items: menu(),
+              )
+            ]),
+            h.main(content),
           ]),
-          h.main(content),
-        ])
+        ],
+      );
+
+  String _head() => h.head([
+        h.meta(charset: 'utf-8'),
+        h.meta(httpEquiv: 'X-UA-Compatible', content: 'IE=edge'),
+        h.meta(
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1.0',
+        ),
+        h.title('Todos'),
+        h.link(
+          rel: h.LinkRel.stylesheet,
+          href:
+              'https://cdn.jsdelivr.net/npm/@shoelace-style/$shoelaceVersion/cdn/themes/light.css',
+        ),
+        h.link(
+          rel: h.LinkRel.stylesheet,
+          href:
+              'https://cdn.jsdelivr.net/npm/@shoelace-style/$shoelaceVersion/cdn/themes/dark.css',
+        ),
+        h.link(
+            rel: h.LinkRel.stylesheet,
+            href: 'http://localhost:8081/styles/prefab.css'),
+        h.script(
+          type: 'module',
+          src:
+              'https://cdn.jsdelivr.net/npm/@shoelace-style/$shoelaceVersion/cdn/shoelace-autoloader.js',
+        ),
+        h.script(src: 'http://localhost:8081/interface.dart.js', defer: true),
       ]);
 
   List<String> list<T>(List<T> items) {
     final entity = registry.lookup<T>();
+    final content = _listContent(entity, items);
+    return [
+      ...content,
+      _addDialog(entity),
+    ];
+  }
+
+  List<String> _listContent<T>(EntitySupport<T> entity, List<T> items) {
     return [
       h.h1(entity.name.plural),
       sl.card([
@@ -59,46 +77,78 @@ abstract class BasicFrontendTemplate {
       sl.card([
         h.table([
           h.thead([
-            h.tr(entity.fieldAccessors.keys
+            h.tr(entity.fields
                 .where((field) => !entity.keyFields.contains(field))
                 .map((field) => h.th([field.sentenceCase]))
                 .toList()),
           ]),
-          h.tbody(items.map((item) => listItem(item, entity)).toList()),
+          h.tbody(items.map((item) => _listItem(item, entity)).toList()),
         ]),
       ]),
-      sl.dialog(
-        label: 'Add ${entity.name.sentenceCase.toLowerCase()}',
-        classes: ['add-dialog'],
-        content: [
-          sl.button(
-            'Cancel',
-            classes: ['cancel'],
-            slot: 'footer',
-          ),
-          sl.button(
-            'Add',
-            variant: sl.ButtonVariant.primary,
-            classes: ['add'],
-            slot: 'footer',
-          ),
-        ],
-      ),
     ];
+  }
+
+  String _addDialog<T>(EntitySupport<T> entity) {
+    return sl.dialog(
+      label: 'Add ${entity.name.sentenceCase.toLowerCase()}',
+      classes: ['add-dialog'],
+      content: [
+        h.form(
+          content: entity.fields
+              .where((field) => !entity.keyFields.contains(field))
+              .map((field) => _fieldInput(field, entity))
+              .toList(),
+        ),
+        sl.button(
+          'Cancel',
+          classes: ['cancel'],
+          slot: 'footer',
+        ),
+        sl.button(
+          'Add',
+          variant: sl.ButtonVariant.primary,
+          classes: ['add'],
+          slot: 'footer',
+        ),
+      ],
+    );
+  }
+
+  String _fieldInput(String field, EntitySupport<dynamic> entity) {
+    return switch (entity.fieldTypes[field]) {
+      String => sl.input(
+          id: 'input-${field.paramCase}',
+          label: field.sentenceCase,
+          name: field,
+          type: h.InputType.text,
+        ),
+      bool => sl.checkbox(
+          field.sentenceCase,
+          id: 'input-${field.paramCase}',
+          name: field,
+        ),
+      DateTime => sl.input(
+          id: 'input-${field.paramCase}',
+          label: field.sentenceCase,
+          name: field,
+          type: h.InputType.date,
+        ),
+      dynamic other => throw ArgumentError('Unsupported type: $other')
+    };
   }
 
   List<String> menu();
 
-  String listItem<T>(T item, EntitySupport<T> entity) {
+  String _listItem<T>(T item, EntitySupport<T> entity) {
     final cells = entity.fieldAccessors.entries
         .where((field) => !entity.keyFields.contains(field.key))
-        .map(
-            (field) => h.td([formatCell(item, field.key, field.value, entity)]))
+        .map((field) =>
+            h.td([_formatCell(item, field.key, field.value, entity)]))
         .toList();
     return h.tr(cells);
   }
 
-  String formatCell<T>(
+  String _formatCell<T>(
     T item,
     String fieldName,
     dynamic Function(T) fieldAccessor,
@@ -110,7 +160,7 @@ abstract class BasicFrontendTemplate {
       num n => n.toString(),
       bool b => sl.checkbox('', checked: b, disabled: true),
       DateTime d => dateFormat.format(d),
-      _ => throw ArgumentError('Unsupported type: ')
+      _ => throw ArgumentError('Unsupported type: ${value.runtimeType}')
     };
   }
 }
--- prefab/lib/src/frontend/basic/web.dart
+++ prefab/lib/src/frontend/basic/web.dart
@@ -5,7 +5,18 @@ import 'package:fabric_prefab/src/frontend/basic/shoelace.dart';
 void initPrefab() {
   final addButton = querySelector('.add-button');
   final addDialog = querySelector('.add-dialog') as SlDialog;
-  final addDialogCloseButton = querySelector('.add-dialog .cancel');
+  final addDialogCancelButton = querySelector('.add-dialog .cancel');
+  final addDialogAddButton = querySelector('.add-dialog .add');
+  final addForm = querySelector('.add-dialog form') as FormElement;
   addButton?.onClick.listen((event) => addDialog.show());
-  addDialogCloseButton?.onClick.listen((event) => addDialog.hide());
+  addDialogCancelButton?.onClick.listen((event) => addDialog.hide());
+  addDialogAddButton?.onClick.listen((event) {
+    _addEntity(addForm);
+    addDialog.hide();
+  });
+}
+
+void _addEntity(FormElement form) {
+  final formData = FormData(form);
+  // TODO: we need a client-side registry to look up values with their respective types and validate.
 }
